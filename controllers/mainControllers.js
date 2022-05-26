5220dcf74b018b72345ad335a4704ce6beed4dce
B-qui Kesting <belenkesting@gmail.com>
2022-05-25 21:14:56 -0300

    Agrego textos dinámicos

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,8 +1,42 @@
+const about = {
+    title: 'Hola mundo',
+    description: 'Esto es una prueba',
+    history: 'Cualquier información',
+    menu: "cosas para elegir de comida"
+}
+
+const menu = [ 
+    {
+        id : 1,
+        title: 'Carpaccio fresco',
+        description: 'Entrada Carpaccio de salmón con cítricos',
+        price: 'U$S 65.50'
+    },
+    {
+        id : 2,
+        title: 'Risotto de berenjena',
+        description: 'Entrada Carpaccio de salmón con cítricos',
+        price: 'U$S 45.50'
+    },
+    {
+        id : 3,
+        title: 'Empanadas salteñas',
+        description: 'De carne cortada al cuchillo',
+        price: 'U$S 15.50'
+    },
+    {
+        id : 4,
+        title: 'Humitas',
+        description: 'Relleno de choclo envuelto en chala',
+        price: 'U$S 25.50'
+    }
+]
+
 const mainController = {
     index: (req, res) => {
-        res.render("index")
+        res.render("index", {about: about, menu: menu})
     },
-    about: (req, res) => {
+    detalle: (req, res) => {
         res.render("detalleMenu")
     }
 }
--- routers/main.js
+++ routers/main.js
@@ -3,6 +3,6 @@ const express = require('express')
 const mainController = require('../controllers/mainControllers')
 const router = express.Router()
 router.get('/', mainController.index)
-router.get('/about', mainController.about)
+router.get('/detalle', mainController.detalle)
 
 module.exports = router
